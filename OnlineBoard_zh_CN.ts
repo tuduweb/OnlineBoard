e3c54363ac678c62fc0f1110ac15777f318595d0
bin <tuduweb@qq.com>
2021-08-08 18:22:23 +0800

    init.

---
+++ OnlineBoard_zh_CN.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>HomeForm.ui</name>
+    <message>
+        <location filename="HomeForm.ui.qml" line="8"/>
+        <source>Home</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="HomeForm.ui.qml" line="11"/>
+        <source>You are on the home page.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Page1Form.ui</name>
+    <message>
+        <location filename="Page1Form.ui.qml" line="8"/>
+        <source>Page 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Page1Form.ui.qml" line="11"/>
+        <source>You are on Page 1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Page2Form.ui</name>
+    <message>
+        <location filename="Page2Form.ui.qml" line="8"/>
+        <source>Page 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Page2Form.ui.qml" line="11"/>
+        <source>You are on Page 2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="9"/>
+        <source>Stack</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="42"/>
+        <source>Page 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="50"/>
+        <source>Page 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
